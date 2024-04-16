84503916ea87d3450ad214dbf149a24e0e99b813
Akshit1507 <akshitg646@gmail.com>
2024-04-16 10:41:01 +0530

    Updated Socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chit-chat-app-18pl.onrender.com/", {
         query: {
           userId: authUser._id,
         },
