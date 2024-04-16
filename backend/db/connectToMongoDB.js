310fbb457370e3e5720037cafd6d119601d7887e
Akshit1507 <akshitg646@gmail.com>
2024-04-16 10:23:13 +0530

    Initial Commit

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Eroor connecting to MongoDB", error.message);
+  }
+};
+export default connectToMongoDB;
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
+const router = express.Router();
+router.get("/", protectRoute, getUsersForSidebar);
+export default router;
